a1810f61a5a601f8e9a7cd07694fad5e8f419189
jonn red <jonnred1123@gmail.com>
2020-09-08 19:49:54 +0800

    practice
    not done yet , i need more time to research about DOM

---
+++ index.js
@@ -0,0 +1,12 @@
+const prevBtn = document.querySelector("#button-left");
+const nextBtn = document.querySelector("#button-right");
+const images = document.querySelector(".image")
+const body = document.querySelector("body")
+
+
+prevBtn.addEventListener("click",
+()=>{
+images.style.background = "url('./pictures/image5.jpg')";
+
+ } )
+    
