e9a2c2edea41fef82a394c83fb4899c955e8aec5
jonn red <jonnred1123@gmail.com>
2020-09-09 11:14:30 +0800

    image slider semi-complete
    there is still a problem in left button, some image are not showing

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const prevBtn = document.querySelector("#button-left");
 const nextBtn = document.querySelector("#button-right");
 const images = document.querySelector(".image")
-const body = document.querySelector("body")
+const image = document.querySelector("#imahe")
+const imageSlides = ["url('./pictures/image1.jpg')",
+"url('./pictures/image2.jpg')",
+"url('./pictures/image3.jpg')",
+"url('./pictures/image4.jpg')",
+"url('./pictures/image5.jpg')"] 
+var currentSlides = 1;
 
+prevBtn.addEventListener("click",()=>{
+        imahe.style.backgroundImage = imageSlides[currentSlides];
+        currentSlides--;
+        console.log(`hey ${currentSlides}`);
+        imagePrev();
+})
+nextBtn.addEventListener("click",()=>{
+    imahe.style.backgroundImage = imageSlides[currentSlides];
+    currentSlides++;
+    console.log(`hey ${currentSlides}`)
+    imagesNext();
 
-prevBtn.addEventListener("click",
-()=>{
-images.style.background = "url('./pictures/image5.jpg')";
+})
 
- } )
-    
+const imagesNext = ()=>{
+    if (imageSlides.length   == currentSlides){
+        currentSlides=0;
+        console.log(`hey${currentSlides}`)
+    }
+}
+const imagePrev = ()=>{
+    if (currentSlides == 0){
+        currentSlides=imageSlides.length;
+        console.log(`hey${currentSlides}`)
+    }
+}
